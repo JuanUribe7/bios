04351b56f5209801dfd0295a4bb4cc11355ad583
JuanUribe7 <120332493+JuanUribe7@users.noreply.github.com>
2024-02-17 20:53:52 -0500

    Delete BIOS PCSX2 directory

--- BIOS PCSX2/SCPH-77008_RUS_Con_0220_20060210_v15.bat
+++
@@ -1,3 +0,0 @@
-@7za.exe a -t7z -mx7 -mse SCPH-77008_RUS_Con_0220_20060210_v15.P2b SCPH-77008_RUS_Con_0220_20060210_v15_[????????].*
-@if exist 7za.exe del /F SCPH-77008_RUS_Con_0220_20060210_v15_[????????].*
-@if exist 7za.exe del /F SCPH-77008_RUS_Con_0220_20060210_v15.bat
